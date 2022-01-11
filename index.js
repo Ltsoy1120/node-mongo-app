e3ea1bb73caea50abe5598f300cb5345b812e00b
Elena Tsoy <ltsoy1120@gmail.com>
2022-01-12 01:14:25 +0600

    added create-form and todo-list with css

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 const exphbs = require("express-handlebars");
-const todoRouts = require("./routes/todos");
+const todoRoutes = require("./routes/todos");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -14,7 +15,10 @@ app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 app.set("views", "views");
 
-app.use(todoRouts);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(todoRoutes);
 
 async function start() {
   try {
